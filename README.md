e667d8351009bd9278f2aea1ee3cfbb05be546e5
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-04-22 14:26:32 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# coundown-timer
+ 
---
+++ app.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+const timer = async () => {
+    const ques = await inquirer.prompt({
+        name: "time",
+        type: "confirm",
+        message: "can you want to start time ?",
+        default: "true"
+    });
+    if (ques.time) {
+        setInterval(() => {
+            let date = new Date();
+            console.clear(); // Clear console to overwrite previous output
+            console.log(date.toLocaleTimeString());
+        }, 1000);
+    }
+};
+timer();
