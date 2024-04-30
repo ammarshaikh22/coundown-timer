6bd47b5ba79f17519f3704812953e7ca6b8daae7
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-04-30 20:23:53 +0500

    vvbc

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 import inquirer from "inquirer";
 const timer = async () => {
-    const ques = await inquirer.prompt({
-        name: "time",
-        type: "confirm",
-        message: "can you want to start time ?",
-        default: "true"
-    });
-    if (ques.time) {
-        setInterval(() => {
-            let date = new Date();
-            console.clear(); // Clear console to overwrite previous output
-            console.log(date.toLocaleTimeString());
-        }, 1000);
+    let condition = true;
+    while (condition) {
+        const ques = await inquirer.prompt({
+            name: "time",
+            type: "confirm",
+            message: "can you want to start time ?",
+            default: "true"
+        });
+        if (!ques.time) {
+            setTimeout(() => {
+                let date = new Date();
+                console.clear(); // Clear console to overwrite previous output
+                console.log(date.toLocaleTimeString());
+            }, 1000);
+        }
     }
 };
 timer();
--- app.ts
+++ app.ts
@@ -1,18 +1,20 @@
+
 import inquirer from "inquirer";
 
 const timer = async () => {
     const ques = await inquirer.prompt({
         name: "time",
         type: "confirm",
-        message:"can you want to start time ?",
-        default:"true"
+        message: "can you want to start time ?",
+        default: "true"
     })
-    if(ques.time){
+    if (ques.time) {
         setInterval(() => {
             let date = new Date()
             console.clear(); // Clear console to overwrite previous output
-            console.log(date.toLocaleTimeString()); 
-        },1000)
+            console.log(date.toLocaleTimeString());
+        }, 1000)
     }
 }
+
 timer()
