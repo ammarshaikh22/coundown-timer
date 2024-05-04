08e2b40d145b76fc1e064708421f48fd6d22bcb7
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-05-05 00:16:39 +0500

    ww

--- app.ts
+++ app.ts
@@ -1,19 +1,35 @@
-
+#!/usr/bin/env node
 import inquirer from "inquirer";
 
 const timer = async () => {
-    const ques = await inquirer.prompt({
-        name: "time",
-        type: "confirm",
-        message: "can you want to start time ?",
-        default: "true"
-    })
-    if (ques.time) {
-        setInterval(() => {
-            let date = new Date()
-            console.clear(); // Clear console to overwrite previous output
-            console.log(date.toLocaleTimeString());
-        }, 1000)
+    let condition = true
+    while (condition) {
+        const ques = await inquirer.prompt([
+            {
+                name: "startingNumber",
+                type: "number",
+                message: "give me the number to start time ? "
+            },
+            {
+                name: "endingNumber",
+                type: "number",
+                message: "give me the number to end time ? "
+            }
+        ])
+        let counter = ques.startingNumber
+        if (ques.startingNumber > 0) {
+            setInterval(() => {
+                if (counter >= ques.endingNumber) {
+                    counter = ques.endingNumber
+                    clearInterval(counter)
+                    condition = false
+                } else {
+                    counter = counter + 1
+                    console.clear()
+                    console.log(counter)
+                }
+            }, 1000)
+        }
     }
 }
 
